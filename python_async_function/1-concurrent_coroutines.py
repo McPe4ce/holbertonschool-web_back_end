ca46e72c71217317dc2c17f726b5914c0ac37fa6
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 14:41:06 +0200

    Add type hints for parameters in wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ from basic_async_syntax import wait_random
 import asyncio
 
 
-async def wait_n(n, max_delay) -> float:
+async def wait_n(n: int, max_delay: int) -> float:
     """ Return the list of all the delays"""
     delays = []
 
