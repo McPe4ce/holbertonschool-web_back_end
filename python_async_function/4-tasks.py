4ae45cabd12156589a79f45759623f1cf4e0c0de
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 17:39:22 +0200

    Add docstring to task_wait_n function for clarity on its purpose

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
+"""The code is nearly identical to wait_n, task_wait_random is being called."""
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Does the same as wait_n"""
     delays = []
     waiter = [task_wait_random(max_delay) for _ in range(n)]
 
