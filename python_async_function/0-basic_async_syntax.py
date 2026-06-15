de5cdca9bbaf4466616e6268c3ae6c77616f1df2
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 12:27:24 +0200

    Add initial implementation of asynchronous function and update README files

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that uses an asynchronous coroutine that prints with a delay"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    da_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(da_delay)
+    return da_delay
