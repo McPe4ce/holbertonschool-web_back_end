23bc908c65b274d1ea8bff15a707c185b8afd6c7
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 14:47:50 +0200

    Refactor import statement for wait_random in 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Script that spawn wait_random n times with the specified max_delay"""
 
-from basic_async_syntax import wait_random
 import asyncio
-
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """ Return the list of all the delays"""
