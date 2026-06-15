c8a4c4e3212499fb8c6c83d9cd51b5cbe8667e10
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 13:22:59 +0200

    Fix formatting of default parameter in wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Function that creates a random delay"""
     da_delay = random.uniform(0, max_delay)
     await asyncio.sleep(da_delay)
