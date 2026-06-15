e068d9a962512fe83b29ae084c80ef53a9ec43cd
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 17:34:23 +0200

    Add 4-tasks.py to implement task_wait_n function for async execution

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    delays = []
+    waiter = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(waiter):
+        delayed = await task
+        delays.append(delayed)
+    return delays
+
+
+if __name__ == "__main__":
+    n = 5
+    max_delay = 6
+    print(asyncio.run(task_wait_n(n, max_delay)))
