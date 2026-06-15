35f41cc24a3974d96f62d834957506bd3f9c7b14
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 16:45:57 +0200

    Add docstring to task_wait_random function for clarity on its purpose

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,5 +5,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Script that returns a task and does use the asyncio.Task"""
     task = asyncio.create_task(wait_random(max_delay))
     return task
