73127d3218089da3d6258c62bfc247f517d09bf7
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-15 16:44:24 +0200

    Refactor 3-tasks.py to remove main test function and clean up code structure

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     task = asyncio.create_task(wait_random(max_delay))
     return task
-
-
-if __name__ == "__main__":
-    async def test(max_delay: int) -> float:
-        task = task_wait_random(max_delay)
-        await task
-        print(task.__class__)
-
-    asyncio.run(test(5))
