bc62649b3a2cb982f039d25ee965745fef5b0b6e
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-16 11:33:54 +0200

    Refactor async_comprehension docstring for improved readability

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,5 +5,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collect 10 random floats from async_generator using an async comprehension."""
+    """Collect 10 random floats from async_generator
+    using an async comprehension."""
     return [num async for num in async_generator()]
