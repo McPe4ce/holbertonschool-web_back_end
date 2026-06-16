1c543ed7c28e577b8b87fc890b5ff0b7792a85b6
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-16 12:18:57 +0200

    Refactor measure_runtime function for improved docstring clarity and type hinting

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,15 +5,14 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
-    """Measures the run time using gather"""
+async def measure_runtime() -> float:
+    """Measure the total runtime of running async_comprehension
+    four times in parallel."""
     start_timer = time.perf_counter()
-
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    total_time = time.perf_counter() - start_timer
-    return total_time
+    return time.perf_counter() - start_timer
