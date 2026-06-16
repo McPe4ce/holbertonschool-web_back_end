a9eda4b3c1b089d940d87e09c88605ef34db6ad3
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-16 11:07:26 +0200

    Refactor async_generator function to improve docstring and yield random floats between 0 and 10

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""Scrip that returns a random num from an async_generator"""
+"""Module that provides an async generator yielding random floats."""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
-    """Scrip that returns a random num from an async_generator"""
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Loop 10 times, wait 1 second asynchronously, then yield a random float between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random()
+        yield random.uniform(0, 10)
