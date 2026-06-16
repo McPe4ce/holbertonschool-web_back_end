368e01703dbee99f8e3e5295374cf59520cadb40
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-16 12:13:02 +0200

    Add measure_runtime module to measure execution time using asyncio.gather

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that measures the run time using gather"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Measures the run time using gather"""
+    start_timer = time.perf_counter()
+    ac = async_comprehension()
+
+    await asyncio.gather(ac, ac, ac, ac)
+    total_time = time.perf_counter() - start_timer
+    return total_time
